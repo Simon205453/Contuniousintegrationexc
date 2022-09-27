b625b7890bff2a40785fadd5339664fa8424d7ed
asgerbirk <asger.birk@gmail.com>
2022-09-27 08:56:16 +0200

    first

--- src/main/java/com/example/contuniousintegrationexc/controller/Controller.java
+++ src/main/java/com/example/contuniousintegrationexc/controller/Controller.java
@@ -1,17 +1,24 @@
 package com.example.contuniousintegrationexc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-
 public class Controller {
 
-    @GetMapping
+    @GetMapping("/")
     public String index(){
-        return "/index";
+        return "index";
     }
+
+    @GetMapping("/asger")
+    public String asger(){
+        return "asger";
+    }
+
+
 
 }
