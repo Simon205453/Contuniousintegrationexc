0ffdce81942c511d9f43d147247a1235755557dc
asgerbirk <asger.birk@gmail.com>
2022-09-27 09:02:11 +0200

    first

--- src/main/java/com/example/contuniousintegrationexc/controller/Controller.java
+++ src/main/java/com/example/contuniousintegrationexc/controller/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@org.springframework.stereotype.Controller
 public class Controller {
 
     @GetMapping("/")
