d25733972703e9de7935745f3b6de68d9e829d51
Meerwaldten <Mikkel_m_j@hotmail.com>
2022-09-27 09:02:31 +0200

    Mikkel er sej

--- src/main/java/com/example/contuniousintegrationexc/controller/Controller.java
+++ src/main/java/com/example/contuniousintegrationexc/controller/Controller.java
@@ -1,17 +1,22 @@
 package com.example.contuniousintegrationexc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
-@RestController
+@org.springframework.stereotype.Controller
 public class Controller {
 
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+
+    @GetMapping("/skerder")
+    public String skerder(){
+        return "skerder";
     }
 
     @GetMapping("/asger")
